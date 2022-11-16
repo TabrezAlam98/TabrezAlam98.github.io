5714cb41ce332ea994fe43ddc75cafb1a785a713
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2022-11-16 23:07:05 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import Hero from "./Components/Hero/Hero";
-
 import  {Navbar}  from "./Components/Navbar";
 import Navbar1 from "./Components/Navbar1";
 import Projects from "./Components/Projects/Projects";
@@ -21,8 +20,6 @@ function App() {
           <section id="about-me">
           <About />
           </section>
-          
-         
           <section id='skills'>
           <Skills  />
           </section>
